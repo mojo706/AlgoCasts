fd7cbd058f573344943f8cf0f764ebf446ea972d
mojo706 <xriseugene@gmail.com>
2020-07-22 05:58:22 +0300

    Solution2 for palindrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,8 +7,12 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
+// function palindrome(str) {
+//   return str.split('').reverse().join('') === str ? true : false
+// }
+
 function palindrome(str) {
-  return str.split('').reverse().join('') === str ? true : false
+  return str.split('').every((char, i) => char === str[str.length -i -1])
 }
 
 module.exports = palindrome;
